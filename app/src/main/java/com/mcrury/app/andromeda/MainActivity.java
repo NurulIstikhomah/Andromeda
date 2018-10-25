46eb4430165f404449e70b1806ac48b4ae54da34
F Masruri <mcrury@gmail.com>
2018-10-25 23:31:50 +0700

    01C - Mengubah TextView dari java

--- app/src/main/java/com/mcrury/app/andromeda/MainActivity.java
+++ app/src/main/java/com/mcrury/app/andromeda/MainActivity.java
@@ -2,6 +2,7 @@ package com.mcrury.app.andromeda;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Mengubah TextView yang sudah ada di layout xml
+        TextView t = findViewById(R.id.tv);
+        t.setText("Ini adalah contoh men-set isi TextView dari file java");
+
     }
 }
