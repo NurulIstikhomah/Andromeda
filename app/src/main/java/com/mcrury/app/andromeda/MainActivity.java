6d196ad69b5df5ef32bf7faf67dd4419447ce669
F Masruri <mcrury@gmail.com>
2018-10-25 23:33:03 +0700

    01D - Membuat layout & ImageView dari java

--- app/src/main/java/com/mcrury/app/andromeda/MainActivity.java
+++ app/src/main/java/com/mcrury/app/andromeda/MainActivity.java
@@ -2,6 +2,8 @@ package com.mcrury.app.andromeda;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -14,6 +16,25 @@ public class MainActivity extends AppCompatActivity {
         // Mengubah TextView yang sudah ada di layout xml
         TextView t = findViewById(R.id.tv);
         t.setText("Ini adalah contoh men-set isi TextView dari file java");
+
+
+        // Membuat ImageView programmatically
+        ImageView gambarku = new ImageView(this);
+        gambarku.setImageResource(R.drawable.android_png_hd);
+
+        // Membuat LinearLayout
+        LinearLayout layoutku = new LinearLayout(this);
+        layoutku.setOrientation(LinearLayout.VERTICAL);
+
+        layoutku.setLayoutParams(
+                new LinearLayout.LayoutParams(
+                        LinearLayout.LayoutParams.MATCH_PARENT,
+                        LinearLayout.LayoutParams.MATCH_PARENT )
+        );
+
+        layoutku.addView(gambarku);
+
+        setContentView(layoutku);
 
     }
 }
