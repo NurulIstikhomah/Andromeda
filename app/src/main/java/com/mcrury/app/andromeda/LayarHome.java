c6cce5d4279d01766415f882b2ed83508767182b
F Masruri <mcrury@gmail.com>
2018-10-29 10:28:49 +0700

    03C - Mengirim intent ke aplikasi lain

--- app/src/main/java/com/mcrury/app/andromeda/LayarHome.java
+++ app/src/main/java/com/mcrury/app/andromeda/LayarHome.java
@@ -3,6 +3,7 @@ package com.mcrury.app.andromeda;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
 public class LayarHome extends AppCompatActivity {
@@ -18,5 +19,24 @@ public class LayarHome extends AppCompatActivity {
         TextView tv_email = (TextView) findViewById(R.id.textView2);
 
         tv_email.setText(email);
+    }
+
+    public void klikShare(View view) {
+
+        // Membuat intent dengan parameter ACTION_SEND untuk mengirim info ke aplikasi lain
+        Intent intentpengirim = new Intent(Intent.ACTION_SEND);
+
+        // Sebagai contoh, kita kirimkan textview alamat email
+        TextView et_email = (TextView)findViewById(R.id.textView2);
+        String inputemail = et_email.getText().toString();
+
+        // Set tipe data email
+        intentpengirim.setType("text/plain");
+
+        // Memasukkan data email ke intent
+        intentpengirim.putExtra(Intent.EXTRA_TEXT,inputemail);
+
+        // Android akan menampilkan opsi aplikasi apa saja yang bisa menerima data text/plain
+        startActivity(intentpengirim);
     }
 }
