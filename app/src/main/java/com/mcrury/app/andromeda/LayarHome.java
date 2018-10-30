3114231cd834bdf34ca229409ca61830ed08d8e5
F Masruri <mcrury@gmail.com>
2018-10-30 12:05:34 +0700

    04A - Menampilkan data sinetron dengan ListView sederhana

--- app/src/main/java/com/mcrury/app/andromeda/LayarHome.java
+++ app/src/main/java/com/mcrury/app/andromeda/LayarHome.java
@@ -4,7 +4,8 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.TextView;
+import android.widget.AdapterView;
+import android.widget.ListView;
 
 public class LayarHome extends AppCompatActivity {
 
@@ -13,30 +14,25 @@ public class LayarHome extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_layar_home);
 
-        Intent intentpenerima = getIntent();
-        String email = intentpenerima.getStringExtra("EXTRA_EMAIL");
+        // Membuat listener on click
+        AdapterView.OnItemClickListener itemClickListener = new AdapterView
+                .OnItemClickListener(){
 
-        TextView tv_email = (TextView) findViewById(R.id.textView2);
+            public void onItemClick(AdapterView<?> listView, View itemView, int position, long
+                    id) {
+                // Membuat item click untuk kategori Drama (posisinya 1, lihat di array
+                // kategori_data di file strings.xml)
+                if(position == 1) {
+                    Intent intent = new Intent(LayarHome.this, LayarKategori.class);
+                    startActivity(intent);
+                }
+            }
+        };
 
-        tv_email.setText(email);
+        // Memasukkan listener ke list kategori
+        ListView listView = (ListView) findViewById(R.id.lv_kategori_street_food);
+        listView.setOnItemClickListener (itemClickListener);
+
     }
 
-    public void klikShare(View view) {
-
-        // Membuat intent dengan parameter ACTION_SEND untuk mengirim info ke aplikasi lain
-        Intent intentpengirim = new Intent(Intent.ACTION_SEND);
-
-        // Sebagai contoh, kita kirimkan textview alamat email
-        TextView et_email = (TextView)findViewById(R.id.textView2);
-        String inputemail = et_email.getText().toString();
-
-        // Set tipe data email
-        intentpengirim.setType("text/plain");
-
-        // Memasukkan data email ke intent
-        intentpengirim.putExtra(Intent.EXTRA_TEXT,inputemail);
-
-        // Android akan menampilkan opsi aplikasi apa saja yang bisa menerima data text/plain
-        startActivity(intentpengirim);
-    }
 }
---
+++ app/src/main/java/com/mcrury/app/andromeda/SinetronModel.java
@@ -0,0 +1,45 @@
+package com.mcrury.app.andromeda;
+
+public class SinetronModel {
+    private String nama;
+    private String deskripsi;
+    private int imageResourceId;
+
+    // Array nama sinetron di kategori drama
+    public static final SinetronModel[] drama = {
+
+            new SinetronModel("Tersandung", "Tersandung merupakan sinetron terpanjang yang pernah" +
+                    " diputar yaitu selama 350 tahun. Sinetron ini mengisahkan seorang waria yang" +
+                    " selalu tersandung saat berjalan, entah itu tersandung batu, tersandung " +
+                    "kayu, tersandung kabel, tersandung polisi tidur, dll. ", R.drawable.img_1),
+            new SinetronModel("Putri Yang Diincar", "Putri Yang Diincar merupakan sinetron dengan" +
+                    " durasi tayang selama 3 jam, dan 3 episode setiap hari setelah makan pagi, siang dan" +
+                    " malam akibat penonton ketagihan. Jika terlambat tayang sekali saja, akan " +
+                    "terjadi kesurupan massal.",
+                    R.drawable.img_2),
+            new SinetronModel("Tukang Haji Naik Bubur", "Menceritakan tentang seorang tukang yang" +
+                    " hobinya naik haji. Setelah naik haji, istrinya jadi pingin " +
+                    "ikut2an naik haji, abis itu mertuanya jadi ikut2an, abis itu tetangga2 " +
+                    "satu persatu jadi ikut2an. Itulah kenapa episodenya panjang sekali.", R
+                    .drawable.img_3)
+    };
+
+    // Setiap data punya nama, deskripsi, dan gambar
+    private SinetronModel(String nama, String deskripsi, int imageResourceId) {
+        this.nama = nama;
+        this.deskripsi = deskripsi;
+        this.imageResourceId = imageResourceId;
+    }
+    public String getDeskripsi() {
+        return deskripsi;
+    }
+    public String getNama() {
+        return nama;
+    }
+    public int getImageResourceId() {
+        return imageResourceId;
+    }
+    public String toString() {
+        return this.nama;
+    }
+}
